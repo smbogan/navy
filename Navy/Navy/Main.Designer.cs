66e584d093d5f34916e026b3c426ad77d563988a
smbogan <smbogan@gmail.com>
2016-12-11 14:50:37 -0600

    initial

---
+++ Navy/Navy/Main.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Navy
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.transactions = new System.Windows.Forms.DataGridView();
+            this.menu = new System.Windows.Forms.ToolStrip();
+            this.add = new System.Windows.Forms.ToolStripButton();
+            this.delete = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.transactions)).BeginInit();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.propertyGrid1.Location = new System.Drawing.Point(475, 12);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(329, 487);
+            this.propertyGrid1.TabIndex = 0;
+            // 
+            // transactions
+            // 
+            this.transactions.AllowUserToAddRows = false;
+            this.transactions.AllowUserToDeleteRows = false;
+            this.transactions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.transactions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.transactions.Location = new System.Drawing.Point(11, 12);
+            this.transactions.Name = "transactions";
+            this.transactions.ReadOnly = true;
+            this.transactions.Size = new System.Drawing.Size(445, 487);
+            this.transactions.TabIndex = 1;
+            // 
+            // menu
+            // 
+            this.menu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.add,
+            this.delete});
+            this.menu.Location = new System.Drawing.Point(0, 513);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(819, 25);
+            this.menu.TabIndex = 2;
+            this.menu.Text = "toolStrip1";
+            // 
+            // add
+            // 
+            this.add.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.add.Image = ((System.Drawing.Image)(resources.GetObject("add.Image")));
+            this.add.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(33, 22);
+            this.add.Text = "Add";
+            // 
+            // delete
+            // 
+            this.delete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.delete.Image = ((System.Drawing.Image)(resources.GetObject("delete.Image")));
+            this.delete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(44, 22);
+            this.delete.Text = "Delete";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 538);
+            this.Controls.Add(this.menu);
+            this.Controls.Add(this.transactions);
+            this.Controls.Add(this.propertyGrid1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.transactions)).EndInit();
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.DataGridView transactions;
+        private System.Windows.Forms.ToolStrip menu;
+        private System.Windows.Forms.ToolStripButton add;
+        private System.Windows.Forms.ToolStripButton delete;
+    }
+}
+
